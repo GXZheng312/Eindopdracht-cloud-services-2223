cd49009c7acee5e354742767285c97187d975275
JackyZheng657 <j.zheng657@hotmail.com>
2023-04-05 23:00:20 +0200

    changed architecture

--- services/user/routes/index.js
+++ services/user/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("hello im the user container")
+  res.send("hello im from user container")
 });
 
 module.exports = router;

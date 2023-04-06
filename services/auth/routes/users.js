e97102acd9ed6648ae8502501b85ccea8dd2397b
JackyZheng657 <j.zheng657@hotmail.com>
2023-04-06 12:46:44 +0200

    works

--- services/auth/routes/users.js
+++ services/auth/routes/users.js
@@ -6,16 +6,41 @@ router.get('/:username', async (req, res, next) => {
     const username = req.params.username;
     const connection = await amqp.connect(process.env.RABBITMQ_URL);
     const channel = await connection.createChannel();
-
+    const key = `test.${username}`
+    const replyKey = key + ".response"
     const exchangeName = "users"
     
+    const correlationId = generateUuid();
+
     await channel.assertExchange(exchangeName, 'topic', {
         durable: false
     });
 
-    channel.publish(exchangeName, "test.whatever", Buffer.from(username));
+    // -> test.admin == test.*
+    // -> test.admin.response
+    const queueName = await channel.assertQueue('', { exclusive: true });
+    await channel.bindQueue(queueName.queue, exchangeName, replyKey);
+
+    channel.consume(queueName.queue, message => {
+        console.log('recieved a reply message from rabbitmq');
+        console.log(message.content.toString())
+        connection.close()
+    })
+ 
+    channel.publish(exchangeName, key, Buffer.from(JSON.stringify({ data: username, correlationId: correlationId, replyTo: replyKey})), {
+        contentType: 'application/json',
+        persistent: true,
+        replyTo: replyKey
+    });
 
     res.send(username);
 });
+
+
+function generateUuid() {
+    return Math.random().toString() +
+           Math.random().toString() +
+           Math.random().toString();
+  }
 
 module.exports = router;
--- services/auth/services/rabbitmq.js
+++ services/auth/services/rabbitmq.js
@@ -11,6 +11,34 @@ const connect = async () => {
   }
 };
 
+const publishToExchange = async (exchangeName, routingKey, data) => {
+  const { channel, connection } = await connect();
+  const responseQueue = await channel.assertQueue('', { exclusive: true });
+
+  await channel.assertExchange(exchangeName, 'topic', { durable: false });
+  await channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(data)), {
+    contentType: 'application/json',
+    persistent: true,
+    replyTo: "test"
+  });
+
+  return responseQueue;
+};
+
+const subscribeToTopic = async (exchangeName, routingKey, callback) => {
+  const { channel } = await connect();
+  const queueName = await channel.assertQueue('', { exclusive: true });
+  
+  await channel.assertExchange(exchangeName, 'topic', { durable: false });
+  await channel.bindQueue(queueName.queue, exchangeName, routingKey);
+
+  await channel.consume(queueName.queue, (message) => {
+    const responseQueue = message.properties.replyTo;
+    callback(JSON.parse(message.content.toString()), responseQueue);
+    channel.ack(message);
+  });
+};
+
 module.exports = {
   connect
 };

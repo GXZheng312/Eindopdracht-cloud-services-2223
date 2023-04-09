7cd8cdb69957e39a93f961553218d3c6d870bd42
Sven <sven.de.bresser@hotmail.com>
2023-04-09 13:24:05 +0200

    querystring toegepast op simple properties or subcollections

--- services/user/routes/users.js
+++ services/user/routes/users.js
@@ -3,31 +3,62 @@ const router = express.Router();
 
 const userRepository = require('../repositories/user');
 const roleRepository = require('../repositories/role');
+const User = require('../models/user');
 
-// user/
-router.get('/:username', async function(req, res, next) {
-  const username = req.params.username;
+router.get('/:username', async (req, res) => {
   try {
-    const user = await userRepository.getUserByUsername(username);
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ message: 'User not found' });
+    const username = req.params.username;
+    const address = req.query.address;
+    
+    let projection = {};
+
+    if (address === 'city') {
+      projection = { 'address.city': 1 };
+    } 
+    if (address === 'street') {
+      projection = { 'address.street': 1 };
+    } 
+    if (address === 'zip') {
+      projection = { 'address.zip': 1 };
+    } 
+
+    const user = await User.findOne({ username }, projection);
+    
+    if (!user) {
+      return res.status(404).send('User not found');
     }
+
+    res.send(user);
   } catch (error) {
-    next(error);
+    res.status(500).send(error.message);
   }
 });
 
+
+// user/
+// router.get('/:username', async function(req, res, next) {
+//   const username = req.params.username;
+//   try {
+//     const user = await userRepository.getUserByUsername(username);
+//     if (user) {
+//       res.status(200).json(user);
+//     } else {
+//       res.status(404).json({ message: 'User not found' });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
 router.post('/', async function(req, res, next) {
-  const { username, password, roleName } = req.body;
+  const { username, password, address, roleName } = req.body;
   try {
     const role = await roleRepository.getRoleByName(roleName);
     if (!role) {
       res.status(400).json({ message: 'Invalid role name' });
       return;
     }
-    const user = await userRepository.createUser(username, password, role._id);
+    const user = await userRepository.createUser(username, password, address, role._id);
     res.status(201).json(user);
   } catch (error) {
     next(error);
